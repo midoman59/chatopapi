bf9a4f21ae2d6fa0c352815783b45d3f4c1c4b35
Amine <moha.mohtaje@gmail.com>
2026-05-06 15:44:22 +0200

    Initial commit - Chatop API et ajout connexion bdd

---
+++ src/main/java/com/chatop/chatopapi/ChatopapiApplication.java
@@ -0,0 +1,13 @@
+package com.chatop.chatopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatopapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatopapiApplication.class, args);
+	}
+
+}
