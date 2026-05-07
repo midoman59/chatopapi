92af57c64557c6f0fcbae5b739b6e35180a356ca
Amine <moha.mohtaje@gmail.com>
2026-05-07 11:00:47 +0200

    feature: ajout la configuration de security filter chain authentication et swagger

--- src/main/java/com/chatop/chatopapi/security/SecurityConfig.java
+++ src/main/java/com/chatop/chatopapi/security/SecurityConfig.java
@@ -1,17 +1,32 @@
 package com.chatop.chatopapi.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig      {
 
-@Bean
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/v3/api-docs", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
 }
